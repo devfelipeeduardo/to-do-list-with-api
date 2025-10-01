1e6eb348a53944d62da106a19bb22981051e86c9
Felipe <felipeeduardofreitasm@gmail.com>
2025-09-30 22:19:52 -0300

    feature: dinamica de adicao e exclusao de tarefas em cada lista concluida.

--- ToDoListFront/to-do-list-front/src/components/List.jsx
+++ ToDoListFront/to-do-list-front/src/components/List.jsx
@@ -1,27 +1,60 @@
+import { useState } from 'react';
 import Task from './Task';
 
 import './style.css';
 
 function List({ title }) {
 
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleAddTask() {
+        if (tasks.length == 9) {
+            alert("Só é possível adicionar até 09 tarefas!")
+            return;
+        }
+        
+        if (newTask.trim() === "") return;
+
+        const taskObject = {
+            id: Date.now(),
+            text: newTask
+        };
+
+        setTasks([...tasks, taskObject]);
+        setNewTask("");
+
+
+    }
+
+    function handleDeleteTask(idToDelete) {
+        setTasks(tasks.filter((_, index) => index!== idToDelete));
+    }
+
     return (
         <div className="list">
             <div className="list-title">{title}</div>
             <div className="add-task-container">
-                <button className="add-task-button">+</button>
-                <input className="add-task-input-text" type="text" placeholder="Insira sua tarefa"></input>
+                <button
+                    className="add-task-button"
+                    onClick={handleAddTask}
+                >+</button>
+
+                <input
+                    className="add-task-input-text"
+                    type="text"
+                    placeholder="Insira sua tarefa"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}></input>
             </div>
             <div className="tasks-container">
-                <Task placeholder="1" />
-                <Task placeholder="2" />
-                <Task placeholder="3" />
-                <Task placeholder="4" />
-                <Task placeholder="5" />
-                <Task placeholder="6" />
-                <Task placeholder="7" />
-                <Task placeholder="8" />
-                <Task placeholder="9" />
-                <Task placeholder="10" />
+                {tasks.map((task, index) => (
+                    <Task
+                        key={task.id}
+                        placeholder={task.text}
+                        handleDeleteTask={() => handleDeleteTask(index)}
+                    />
+                ))}
             </div>
         </div>
     );
--- ToDoListFront/to-do-list-front/src/components/Task.jsx
+++ ToDoListFront/to-do-list-front/src/components/Task.jsx
@@ -1,15 +1,16 @@
-function Task({ placeholder }) {
-
+function Task({ placeholder, handleDeleteTask}) {
 
     return (
         <>
             <div className="task-row-container">
                 <input className="task-input" type="text" placeholder={placeholder} />
-                <button className="delete-task-button">x</button>
+                <button
+                className="delete-task-button"
+                onClick={handleDeleteTask}
+                >x</button>
             </div>
         </>
     )
-
 }
 
 export default Task;
