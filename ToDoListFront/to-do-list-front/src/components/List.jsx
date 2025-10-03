f58015efc1dccbe3fa91f5578676a038b5cd05ca
Felipe <felipeeduardofreitasm@gmail.com>
2025-10-02 22:21:10 -0300

    feat: agora quando você escreve e clica Enter, a tarefa é criada. Também agora quando você clica em Enter ou no botão de adicionar, o foco volta para o input

--- ToDoListFront/to-do-list-front/src/components/List.jsx
+++ ToDoListFront/to-do-list-front/src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Task from './Task';
 
 import './style.css';
@@ -7,13 +7,14 @@ function List({ title }) {
 
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
+    const inputRef = useRef(null);
 
     function handleAddTask() {
         if (tasks.length == 9) {
             alert("Só é possível adicionar até 09 tarefas!")
             return;
         }
-        
+
         if (newTask.trim() === "") return;
 
         const taskObject = {
@@ -23,13 +24,18 @@ function List({ title }) {
 
         setTasks([...tasks, taskObject]);
         setNewTask("");
-
-
     }
 
     function handleDeleteTask(idToDelete) {
-        setTasks(tasks.filter((_, index) => index!== idToDelete));
+        setTasks(tasks.filter((_, index) => index !== idToDelete));
     }
+
+    const focusOnInput = () => {
+        console.log(inputRef.current.value);
+        inputRef.current.value = "";
+        inputRef.current.focus(); // volta o foco
+    };
+
 
     return (
         <div className="list">
@@ -37,15 +43,24 @@ function List({ title }) {
             <div className="add-task-container">
                 <button
                     className="add-task-button"
-                    onClick={handleAddTask}
+                    onClick={() => { handleAddTask(); focusOnInput(); }}
                 >+</button>
 
                 <input
+                    ref={inputRef}
                     className="add-task-input-text"
                     type="text"
                     placeholder="Insira sua tarefa"
                     value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}></input>
+                    onChange={(e) => setNewTask(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleAddTask();
+                        }
+                    }}
+                    >
+                </input>
+
             </div>
             <div className="tasks-container">
                 {tasks.map((task, index) => (
